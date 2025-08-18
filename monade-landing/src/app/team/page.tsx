62bdef8647a3b6b726d19ff637a00e876b3ffcd4
Narayan Thakur <thakurnarayan4505@gmail.com>
2025-08-19 02:22:43 +0530

    Update team profiles with correct social links

--- monade-landing/src/app/team/page.tsx
+++ monade-landing/src/app/team/page.tsx
@@ -18,32 +18,32 @@ export default function TeamPage() {
             <Image src="/team/Narayan%20Photo.jpg" alt="Narayan Thakur" width={200} height={200} />
             <p>Narayan Thakur</p>
             <div className="social-links">
-              <a href="https://www.linkedin.com/in/shashwat-yashasvi-4082562a1/" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{" "}
-              <a href="https://x.com/shashwatya71079" target="_blank" rel="noopener noreferrer">Twitter</a>
+              <a href="https://www.linkedin.com/in/narayan-thakur-b63b55281/" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{" "}
+              <a href="https://x.com/thakurnarayan49" target="_blank" rel="noopener noreferrer">Twitter</a>
             </div>
           </div>
           <div className="team-member">
             <Image src="/team/Amol%20Photo.jpg" alt="Amol Soans" width={200} height={200} />
             <p>Amol Soans</p>
             <div className="social-links">
-              <a href="https://www.linkedin.com/in/shashwat-yashasvi-4082562a1/" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{" "}
-              <a href="https://x.com/shashwatya71079" target="_blank" rel="noopener noreferrer">Twitter</a>
+              <a href="https://www.linkedin.com/in/amoldericksoans/" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{" "}
+              <a href="https://x.com/AmolSoans" target="_blank" rel="noopener noreferrer">Twitter</a>
             </div>
           </div>
           <div className="team-member">
             <Image src="/team/Screenshot%202025-08-18%20141219.png" alt="Aaditya Rangarajan" width={200} height={200} />
             <p>Aaditya Rangarajan</p>
             <div className="social-links">
-              <a href="https://www.linkedin.com/in/shashwat-yashasvi-4082562a1/" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{" "}
-              <a href="https://x.com/shashwatya71079" target="_blank" rel="noopener noreferrer">Twitter</a>
+              <a href="hhttps://www.linkedin.com/in/aaditya2200/" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{" "}
+              <a href="https://x.com/ADR_ad22" target="_blank" rel="noopener noreferrer">Twitter</a>
             </div>
           </div>
           <div className="team-member">
             <Image src="/team/shashwat.jpg" alt="Shashwat Yashasvi" width={200} height={200} />
             <p>Shashwat Yashasvi</p>
             <div className="social-links">
-              <a href="#" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{" "}
-              <a href="#" target="_blank" rel="noopener noreferrer">Twitter</a>
+              <a href="https://www.linkedin.com/in/shashwat-yashasvi-4082562a1/" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{" "}
+              <a href="https://x.com/shashwatya71079" target="_blank" rel="noopener noreferrer">Twitter</a>
             </div>
           </div>
         </div>
